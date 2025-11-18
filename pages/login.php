e7b1f8c97b0ea4a6698f9966caced96c4c127aa7
Wagner Augusto <wagneraugustojr09@gmail.com>
2025-11-18 17:17:08 -0300

    Add files via upload

--- pages/login.php
+++ pages/login.php
@@ -1,23 +1,16 @@
 <?php
 session_start();
+if(isset($_SESSION['user'])){
+  if($_SESSION['user']['perfil'] === 'master'){
+    header("Location: consulta_usuarios.php");
+  } else {
+    header("Location: menucomum.html");
+  }
+  exit;
+}
 
 $erro = $_GET['erro'] ?? '';
-
-if (isset($_SESSION['user_temp'])) {
-    header("Location: 2fa.php");
-    exit;
-}
-
-if (isset($_SESSION['user'])) {
-    if ($_SESSION['user']['perfil'] === 'master') {
-        header("Location: consulta_usuarios.php");
-    } else {
-        header("Location: menucomum.html");
-    }
-    exit;
-}
-?>  
-
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>

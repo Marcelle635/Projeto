3646fb160b04f69958f14c69186dfa4b02532607
Wagner Augusto <wagneraugustojr09@gmail.com>
2025-11-25 15:01:58 -0300

    Update logs.php

--- pages/logs.php
+++ pages/logs.php
@@ -1,4 +1,6 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
+
 session_start();
 require_once("../config/db.php");
 
@@ -387,4 +389,5 @@ document.addEventListener("DOMContentLoaded", function () {
         </div>
     </footer>
 </body>
+
 </html>
